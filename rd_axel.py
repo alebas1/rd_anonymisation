deb752bc5abd41a97b943b0dcfd80db423862b32
VRoyMobivia <vroy@gmail.com>
2021-12-08 15:54:56 +0100

    ano des mots proche des mots detecter (à 20 pixel)

--- rd_axel.py
+++ rd_axel.py
@@ -11,6 +11,7 @@ def extract_text_data(image_data):
     text_boxes_arr = []
 
     # find customer box
+    index=0
     for x, b in enumerate(image_data.splitlines()):
         if x != 0:
             b = b.split()
@@ -20,8 +21,10 @@ def extract_text_data(image_data):
                     'top': int(b[7]),
                     'width': int(b[8]),
                     'height': int(b[9]),
-                    'text': b[11]
+                    'text': b[11],
+                    'index': index
                 })
+                index+=1
 
     return text_boxes_arr
 
@@ -34,9 +37,24 @@ def check_regex(text_boxes_arr):
     return valid_text_boxes_arr
 
 
-def anonymize_text(img, boxes):
-    for b in boxes:
+def anonymize_text(img, ano_boxes, boxes):
+    for b in ano_boxes:
         cv2.rectangle(img, (b["left"], b["top"]), (b["left"] + b["width"], b["top"] + b["height"]), (0, 0, 0), -1)
+
+        #looking forward
+        i=b["index"]
+        while((boxes[i+1]["left"]<=(boxes[i]["left"]+boxes[i]["width"])+20) and (boxes[i+1]["top"]==boxes[i]["top"])):
+            tmp=boxes[i+1]
+            cv2.rectangle(img, (tmp["left"], tmp["top"]), (tmp["left"] + tmp["width"], tmp["top"] + tmp["height"]), (0, 0, 0), -1)
+            i+=1
+
+        #looking backward
+        i=b["index"]
+        while((boxes[i-1]["left"]<=(boxes[i]["left"]+boxes[i]["width"])+20) and (boxes[i-1]["top"]==boxes[i]["top"])):
+            tmp=boxes[i-1]
+            cv2.rectangle(img, (tmp["left"], tmp["top"]), (tmp["left"] + tmp["width"], tmp["top"] + tmp["height"]), (0, 0, 0), -1)
+            i-=1
+
     cv2.imwrite('./image_output.jpg', img)
 
 
@@ -49,6 +67,9 @@ if __name__ == '__main__':
     image_data = pytesseract.image_to_data(image)
 
     # print(extract_text_data(image_data))
-    valid_ano = check_regex(extract_text_data(image_data))
+    text_data = extract_text_data(image_data)
+    #for s in text_data:
+    #    print(s)
+    valid_ano = check_regex(text_data)
 
-    anonymize_text(image, valid_ano)
+    anonymize_text(image, valid_ano, text_data)
