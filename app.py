b23d2819e139a51211a185344347bb24c483fb3b
Axel Lebas <lebas.axel@gmail.com>
2021-12-09 15:20:59 +0100

    refactoring code

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # regex
 import re
 
@@ -7,12 +9,12 @@ import pytesseract
 
 import regex_config
 
+
 def extract_text_data(image_data):
     text_boxes_arr = []
 
-    # find customer box
-    index=0
-    for x, b in enumerate(image_data.splitlines()):
+    index = 0
+    for (x, b) in enumerate(image_data.splitlines()):
         if x != 0:
             b = b.split()
             if len(b) == 12:
@@ -22,9 +24,9 @@ def extract_text_data(image_data):
                     'width': int(b[8]),
                     'height': int(b[9]),
                     'text': b[11],
-                    'index': index
-                })
-                index+=1
+                    'index': index,
+                    })
+                index += 1
 
     return text_boxes_arr
 
@@ -36,53 +38,60 @@ def check_regex(text_boxes_arr):
             valid_text_boxes_arr.append(t)
     return valid_text_boxes_arr
 
+
 def anonymize_text(img, ano_boxes, boxes):
     for b in ano_boxes:
-        cv2.rectangle(img, (b["left"], b["top"]), (b["left"] + b["width"], b["top"] + b["height"]), (0, 0, 0), -1)
+        cv2.rectangle(img, (b['left'], b['top']), (b['left'] + b['width'
+                      ], b['top'] + b['height']), (0, 0, 0), -1)
 
-        #looking forward
-        i=b["index"]
-        while(check_forward(boxes[i], boxes[i+1], i) and check_align(boxes[i], boxes[i+1], i)):
-            tmp=boxes[i+1]
-            cv2.rectangle(img, (tmp["left"], tmp["top"]), (tmp["left"] + tmp["width"], tmp["top"] + tmp["height"]), (0, 0, 0), -1)
-            i+=1
+        # looking forward
+        i = b['index']
+        while check_word_forward(boxes[i], boxes[i + 1], i) \
+            and check_word_aligned(boxes[i], boxes[i + 1], i):
+            box_next = boxes[i + 1]
+            draw_anonymizing_rectangle(img, box_next)
+            i += 1
 
-        #looking backward
-        i=b["index"]
-        while(check_backward(boxes[i], boxes[i-1], i) and check_align(boxes[i], boxes[i-1], i)):
-            tmp=boxes[i-1]
-            cv2.rectangle(img, (tmp["left"], tmp["top"]), (tmp["left"] + tmp["width"], tmp["top"] + tmp["height"]), (0, 0, 0), -1)
-            i-=1
+        # looking backward
+        i = b['index']
+        while check_word_backward(boxes[i], boxes[i - 1], i) \
+            and check_word_aligned(boxes[i], boxes[i - 1], i):
+            box_prev = boxes[i - 1]
+            draw_anonymizing_rectangle(img, box_prev)
+            i -= 1
 
     cv2.imwrite('./image_output.jpg', img)
 
 
-def check_forward(box_curr, box_next, i):
-    return (box_next["left"] <= (box_curr["left"] + box_curr["width"]) + 20)
+def draw_anonymizing_rectangle(img, box):
+    cv2.rectangle(img, (box['left'], box['top']), (box['left']
+                  + box['width'], box['top'] + box['height']), (0, 0,
+                  0), -1)
 
-def check_backward(box_curr, box_prev, i):
-    return (box_prev["left"]+box_prev["width"] >= box_curr["left"] - 20)
 
-def check_align(box_curr, box_next, i):
-    return (box_next["top"] >= box_curr["top"] - 5 or box_next["top"] <= box_curr["top"] + 5)
+def check_word_forward(box_curr, box_next, i):
+    return box_next['left'] <= box_curr['left'] + box_curr['width'] + 20
+
+
+def check_word_backward(box_curr, box_prev, i):
+    return box_prev['left'] + box_prev['width'] >= box_curr['left'] - 20
+
+
+def check_word_aligned(box_curr, box_next, i):
+    return box_next['top'] >= box_curr['top'] - 5 or box_next['top'] \
+        <= box_curr['top'] + 5
 
 
 if __name__ == '__main__':
 
-    #filename = 'factures/midas.jpg'
     filename = 'factures/midas.jpg'
 
     image = cv2.imread(filename)
 
-    # print(pytesseract.image_to_string(image))
     image_data = pytesseract.image_to_data(image)
 
-    # print(extract_text_data(image_data))
     text_data = extract_text_data(image_data)
-    for s in text_data:
-        print(s)
+
     valid_ano = check_regex(text_data)
-    #for s in valid_ano:
-    #    print(s)
 
     anonymize_text(image, valid_ano, text_data)
