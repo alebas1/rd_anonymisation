204e4a87a3f3faf6e168552c2a2939604cecb861
VRoyMobivia <vroy@gmail.com>
2021-12-17 14:24:49 +0100

    plus de regex et regexKeep ajouter

--- regex_config.py
+++ regex_config.py
@@ -1,1 +1,3 @@
-regex = r'(client|monsieur|madame|mademoiselle|m|mr|mme|mlle|mle|rue|boulevard|bld|avenue|all(é|e)e|place|impasse)(\.|\:)?$'
+regex = r'(client|monsieur|madame|mademoiselle|mr|mlle|mle|rue|boulevard|bld|avenue|all(é|e)e|place|impasse|tel|tél)(\.|\:)?$'
+
+regexKepp = r'(Kilometrage|Kilométrage|Km|siret|Siret|SIRET|RC|immat|immatriculation|MEC|circulation)'
