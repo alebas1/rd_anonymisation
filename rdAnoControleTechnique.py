42d313509d9cd1d3de106bf3801ec90cc8871282
VRoyMobivia <vroy@gmail.com>
2022-01-11 15:54:22 +0100

    traitement controle technique

--- rdAnoControleTechnique.py
+++ rdAnoControleTechnique.py
@@ -32,23 +32,60 @@ def extract_text_data(image_data):
 
     return text_boxes_arr
 
+def searchRegex(regex, strToTest) -> bool:
+    regex = regex.replace('i','i*')
+    regex = regex.replace('I','I*')
+    regex = regex.replace('j','j*')
+    regex = regex.replace('J','J*')
+    regex = regex.replace('O','O*')
+    regex = regex.replace('o','o*')
+    regex = regex.replace('ô','ô*')
+    regex = regex.replace('é','é*')
+    #permet de marquer le char originaux et eviter i -> (i|j) -> (i|(j|i)) 
 
-def check_regex(text_boxes_arr, img):
-    #blocks_to_add = []
-    valid_text_boxes_arr = []
-    for t in text_boxes_arr:
-        print(t)
-        if re.search(regex_config.regexControleTechHeaders, t['text'], re.IGNORECASE) and t['top'] <= img.shape[0] * 0.33:
-            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            valid_text_boxes_arr.append(t)
-            #blocks_to_add.append(t['block_number'])
+    regex = regex.replace('i*','(i|j)')
+    regex = regex.replace('I*','(I|J)')
+    regex = regex.replace('j*','(j|i)')
+    regex = regex.replace('J*','(J|I)')
+    regex = regex.replace('O*','(O|o|0)')
+    regex = regex.replace('o*','(o|O|0)')
+    regex = regex.replace('ô*','(ô|o|O|0)')
+    regex = regex.replace('é*','(é|e)')
+    #print('!!!!!!!!!!!!!!!!!!!!!!!! : ' + regex )
+    return re.search(regex,strToTest,re.IGNORECASE)!=None
 
-    # for block_number in blocks_to_add:
-    #     for t in text_boxes_arr:
-    #         if t['block_number'] == block_number:
-    #             valid_text_boxes_arr.append(t)    
+def anonymize_controleur_block(image,text_data,box):
+    i = box['index']+1
 
-    return valid_text_boxes_arr
+    while(not searchRegex('(nom|et|prénom|n|agrément)',text_data[i]['text'])):
+        i+=1
+
+    while(not searchRegex('(signature|identification|information)',text_data[i]['text'])):
+        draw_anonymizing_rectangle(image,text_data[i])
+        i+=1
+
+    return searchRegex('identification|du',text_data[i]['text'])
+    
+
+def anonymize_client_block(image,text_data,box):
+    i = box['index']+1
+
+    while(not searchRegex('(nom|prénom|raison|sociale)',text_data[i]['text'])):
+        i+=1
+
+    while(not searchRegex('résultat',text_data[i]['text'])):
+        draw_anonymizing_rectangle(image,text_data[i])
+        i+=1
+
+def anonymize_text(image,text_data):
+    img=image.copy()
+    for b in text_data:
+        if searchRegex('(contrôleur|identité)',b['text']):
+            if anonymize_controleur_block(img   ,text_data,b): #si return true, alors la facture date d'apres 2018 et n'a pas besoin de traitement suplémentaire
+                break   
+        if searchRegex('titulaire',b['text']):
+            anonymize_client_block(img,text_data,b)
+    return img
 
 
 def anonymize_list(list_ano,img):
@@ -72,12 +109,12 @@ def generateImg(image, file_name):
     image_data = pytesseract.image_to_data(image)
     text_data = extract_text_data(image_data)
 
-    valid_ano = check_regex(text_data, image)
+    #valid_ano = check_regex(text_data, image)
 
     # for s in text_data:
     #        print(s)
 
-    cv2.imwrite(RESULT_PATH  + file_name + '_2ano.jpg', anonymize_list(valid_ano, image))
+    cv2.imwrite(RESULT_PATH  + file_name + '_2ano.jpg', anonymize_text(image,text_data))
 
 if __name__ == '__main__':
 
